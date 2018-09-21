c35fabdbf5e020d8927d083dba846eab80d26e79
Matthew Valli <matthew.valli@gmail.com>
2018-09-21 01:42:37 -0700

    Added Error Management Redux Services

---
+++ src/redux/Error/index.js
@@ -0,0 +1,10 @@
+import ERROR_ACTIONS from './constants';
+import ErrorActionCreator from './actionCreators';
+import ErrorReducer from './reducers';
+
+
+export default {
+    ERROR_ACTIONS,
+    ErrorActionCreator,
+    ErrorReducer,
+}
