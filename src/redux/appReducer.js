81a06df3f732b46ed77806e5d6404eb7c6a3d36c
Matthew Valli <matthew.valli@gmail.com>
2018-09-21 02:01:06 -0700

    Added Initial State and AppReducer Redux Services

--- src/redux/appReducer.js
+++ src/redux/appReducer.js
@@ -1,21 +1,33 @@
 // REDUX
 import { combineReducers } from 'redux';
 // APP REDUCERS
+// Auth
+import RegistrationReducer from "./Registration";
 import AuthenticationReducer from './Authentication';
 import AuthorizationReducer from './Authorization';
-import NotificationReducer from './Notification';
-import InvitationReducer from './Invitation';
-import CartReducer from './Cart';
-import FriendReducer from './Friend';
-import FriendRequestReducer from './FreindRequest';
-import ReminderReducer from './Reminder';
-import RewardReducer from './Reward';
-import OrderReducer from './Order';
-
-import UserReducer from './UserReducer';
+// User Profile Settings
 import UserPreferenceReducer from './UserPreference';
-import ErrorReducer from './ErrorReducer';
-import RegistrationReducer from "./Registration";
+import UserProfileReducer from './UserProfile';
+import InterestReducer from './InterestReducer';
+// Notifications
+import NotificationReducer from './Notification';
+import ReminderReducer from './Reminder';
+// Gamification
+import PointReducer from './Point';
+import RewardReducer from './Reward';
+// Social Network
+import InvitationReducer from './Invitation';
+import FriendReducer from './Friend';
+import FriendRequestReducer from './FriendRequest';
+// Ecommerce
+import CartReducer from './Cart';
+import OrderReducer from './Order';
+import PaymentReducer from './Payment';
+import ReceiptReducer from './Receipt';
+// Error Management
+import ErrorReducer from './Error';
+// UI Management
+
 
 export default function(state, action) {
     return {
@@ -24,8 +36,10 @@ export default function(state, action) {
         authentication: AuthenticationReducer(state.authentication, action),
         authorization: AuthorizationReducer(state.authorization, action),
 
-        // User Settings
+        // UserProfile Settings
         userPreferences: UserPreferenceReducer(state.userPreferences, action),
+        userProfile: UserProfileReducer(state.userProfile, action),
+        interests: InterestReducer(state.interests, action),
 
         // Gamification
         points: PointReducer(state.gamification.points, action),
@@ -43,14 +57,14 @@ export default function(state, action) {
         // Ecommerce
         cart: CartReducer(state.ecommerce.cart, action),
         orders: OrderReducer(state.ecommerce.orders, action),
-        purchases: PurchaseReducer(state.ecommerce.purchases, action),
-        repeipts: ReceiptReducer(state.ecommerce.reciepts, acction),
+        payments: PaymentReducer(state.ecommerce.payments, action),
+        receipts: ReceiptReducer(state.ecommerce.receipts, action),
 
         // Error Management
         errors: ErrorReducer(state.errors, action),
 
         // UI Management
-        loaders: LoaderReducer(state.loaders, action),
-        components: ComponentReducer(state.components, action)
+        // loaders: LoaderReducer(state.loaders, action),
+        // components: ComponentReducer(state.components, action)
     }
 }
