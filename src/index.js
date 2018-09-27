8ff6919fc9448c48faa33f8e25ea6d0e5a3dc2b7
Steph <steph@huynhicode.com>
2018-09-26 21:28:11 -0700

    Added Proxima Nova font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 import { makeMainRoutes } from './routes';
 
 const routes = makeMainRoutes();
